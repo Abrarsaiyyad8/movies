f9f70a40ba37988b4d9150d84e27730c4c1d1240
abrar saiyyad <abrarsaiyyad9421@gmail.com>
2026-03-03 14:48:12 +0530

    from one pice world

---
+++ luffy.sh
@@ -0,0 +1,5 @@
+sudo apt update -y 
+sudo apt-get install nginx -y 
+systemctl start nginx 
+systemctl status nginx 
+systemctl restart nginx 
---
+++ sanjhi.py
@@ -0,0 +1,3 @@
+ab= "hey sanjhi is good chef and it can server you"
+
+print (ab)
